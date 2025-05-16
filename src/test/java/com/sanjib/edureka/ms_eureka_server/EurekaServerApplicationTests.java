580d7b98cae07d5845837b1db60bd43323dfcee5
sanjib <sanjibk@gmail.com>
2025-05-16 13:41:58 +0530

    Edureka

---
+++ src/test/java/com/sanjib/edureka/ms_eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjib.edureka.ms_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
